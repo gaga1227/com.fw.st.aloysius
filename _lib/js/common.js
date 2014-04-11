427eedcef6fb3600bf2f24a2b2ec5f4a24aaef01
Johnny Xu <gaga1227@gmail.com>
2014-04-11 17:51:23 +1000

    m2.1 (100%)
    - completed default content page shell

--- _lib/js/common.js
+++ _lib/js/common.js
@@ -2,20 +2,20 @@
 /* webfonts */
 /* ------------------------------------------------------------------------------ */
 WebFontConfig = { 
-	google: 		{ families: [ 'Asap:400,700,400italic,700italic:latin' ] },
-	loading: 		function() { console.log('[WF] loading'); 	WebFontUtils.onWFLoading(); },
-	active: 		function() { console.log('[WF] active'); 	WebFontUtils.onWFActive(); 	 WebFontUtils.onWFComplete(); },
+	google: 			{ families: [ 'Asap:400,700,400italic,700italic' ] },
+	loading: 			function() { console.log('[WF] loading'); 	WebFontUtils.onWFLoading(); },
+	active: 			function() { console.log('[WF] active'); 	WebFontUtils.onWFActive(); 	 WebFontUtils.onWFComplete(); },
 	inactive: 		function() { console.log('[WF] inactive'); 	WebFontUtils.onWFInactive(); WebFontUtils.onWFComplete(); },
 	fontloading: 	function( familyName, fvd ) { console.log( '[WF] ' + familyName, fvd, 'loading' ); },
 	fontactive: 	function( familyName, fvd ) { console.log( '[WF] ' + familyName, fvd, 'active' ); },
-	fontinactive: 	function( familyName, fvd ) { console.log( '[WF] ' + familyName, fvd, 'inactive' ); },
-	timeout: 		5000
+	fontinactive: function( familyName, fvd ) { console.log( '[WF] ' + familyName, fvd, 'inactive' ); },
+	timeout: 			5000
 };
 WebFontUtils = {
 	onWFLoading: 	function()	{},
-	onWFComplete: 	function()	{},
+	onWFComplete: function()	{},
 	onWFActive: 	function()	{},
-	onWFInactive: 	function()	{}
+	onWFInactive: function()	{}
 }
 /* ------------------------------------------------------------------------------ */
 /* init */
